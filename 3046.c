8edb2b9abb20ef87fdd86ef691d5d9d1632cfbb0
sonjungwoo <sonsung0806@gmail.com>
2024-01-20 10:24:00 +0900

    [BOJ] 3046 bronze4

---
+++ 3046.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int re,er;
+    int result;
+    scanf("%d %d", &re, &result);
+    
+    er = (result*2)-re;
+    
+    printf("%d", er);
+    return 0;
+}
+
+
---
+++ 3046.py
@@ -0,0 +1,3 @@
+a = input()
+b = list(map(int,a.split()))
+print(b[1]*2 - b[0])
