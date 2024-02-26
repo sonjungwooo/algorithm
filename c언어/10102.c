1dd689e7950304b4b08e1d1bf3b5253c09942f5e
sonjungwoo <sonsung0806@gmail.com>
2024-02-26 11:36:15 +0900

    [BOJ] bronze3 10102

---
+++ c언어/10102.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int v;
+    scanf("%d", &v);
+    char a[15];
+    int as = 0;
+    int bs= 0;
+    for (int i=0; i<v; i++) {
+        scanf("%s", a);
+        if (a[i] == 'A') as+=1;
+        else bs+=1;
+    }
+    if (as>bs) printf("A");
+    else if (as<bs) printf("B");
+    else printf("Tie");
+    
+}
